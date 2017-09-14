d6c1870d61b93856fee678f67bae5422e9433065
David Franklin <dave5801@gmail.com>
2017-09-13 17:03:53 -0700

    author filter

--- david-gabriel/scripts/articleView.js
+++ david-gabriel/scripts/articleView.js
@@ -37,9 +37,13 @@ articleView.handleAuthorFilter = function() {
     //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
     //         onto it.
     if ($(this).val()) {
-      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
+      // DONE: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+     // $('#author-filter').filter($(this));
+     $('article').hide();
+     $('article[data-author="'+$(this).val()+'"]').show();
+     
 
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
@@ -84,5 +88,6 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
 })
