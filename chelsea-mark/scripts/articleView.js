ed8905c42e486c3c3d97289c65bc945d8db25634
CHELSEA DOLE <chelseadole@gmail.com>
2017-09-14 09:43:43 -0700

    TODO 3, working on author handler fn

--- chelsea-mark/scripts/articleView.js
+++ chelsea-mark/scripts/articleView.js
@@ -39,6 +39,8 @@ articleView.handleAuthorFilter = function() {
     //         defining. "$(this)" is using jQuery to select that element, so we can chain jQuery methods
     //         onto it.
     if ($(this).val()) {
+
+      if (article.author != $(this).val())
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
