84d50da69a06fa0bd7b93f756c5dd9c943529f99
gabrielx52 <gabriel.meringolo@gmail.com>
2017-09-14 08:53:57 -0700

    Finished handleMainNav function.

--- david-gabriel/scripts/articleView.js
+++ david-gabriel/scripts/articleView.js
@@ -55,7 +55,7 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+  // DONE: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
@@ -73,18 +73,15 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  // DONE: Add an event handler to .main-nav elements that will power the Tabs feature.
   //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
-  $('.main-nav').on('click', function() {
-    console.log('here');
+  $('.tab').on('click', function() {
     $('.tab-content').hide();
-    $('.tab-content[id="'+$(this).val()+'"]').show();
+    $('.tab-content[id="'+$(this).attr('data-content')+'"]').show();
   })
-
-
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
 
