1d83feb4c28e93e5afc0d0e2305ce756cd1823bb
maxawolff <maxawolff@hotmail.com>
2017-09-14 09:28:09 -0700

    we added event listeners to main nav tabs so that when they are clicked they only show the relevent sections

--- davidl-max/starter-code/scripts/articleView.js
+++ davidl-max/starter-code/scripts/articleView.js
@@ -75,6 +75,11 @@ articleView.handleMainNav = function() {
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
+  $('.tab').on('click', function(){
+    $('main section').hide();
+    $('main section[id|=this.data-content]').show().fadeIn();
+  })
+
 
 
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
