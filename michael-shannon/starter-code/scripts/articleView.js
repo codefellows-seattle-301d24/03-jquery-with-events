947949de7dd24f1b75698abd0a1fcd7ab5818656
Michael Shinners <mishinners@icloud.com>
2017-09-14 10:26:13 -0700

    3 TODOs done

--- michael-shannon/starter-code/scripts/articleView.js
+++ michael-shannon/starter-code/scripts/articleView.js
@@ -51,13 +51,14 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
-    if ($(this).val()) {
-
   // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
+  $('#category-filter').on('change', function() {
+    console.log('it wokrs');
+    if ($(this).val()) {
+      $('article').children().hide();
       $('#author-filter').val('');
     }
 
@@ -87,7 +88,11 @@ articleView.setTeasers = function() {
 
 };
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
+// DONE: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
+  articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
 })
